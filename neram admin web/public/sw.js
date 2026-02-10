7b5284fe81ce5c55ece000ba8cbb1d236eaa9aec
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-02-11 00:09:03 +0530

    Refactor desktop CSS to support dark mode with semantic variables

--- neram admin web/public/sw.js
+++
@@ -1,9 +0,0 @@
-// Minimal Service Worker to satisfy PWA requirements
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // Required to be an installable PWA
-  event.respondWith(fetch(event.request));
-});
