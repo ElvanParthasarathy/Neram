9c8e9500a9b794d321fd199712eb987a979487d0
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-03-12 22:09:13 +0530

    chore: bump version code to 9 and version name to 1.7.1 for Play Store release

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.elvan.neram"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.7.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
