0d7bf2574340e66cb7a3f2d661f68ae5331ebbd1
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-03-29 01:54:18 +0530

    chore: bump version to 1.7.7 and add student apk for easy download

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.elvan.neram"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.7.6"
+        versionCode = 15
+        versionName = "1.7.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
